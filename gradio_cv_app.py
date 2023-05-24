452d64e7df4a80c9ea92e1e70029e781db169e7e
Hong Tang <53389274+stanghong@users.noreply.github.com>
2023-05-24 09:00:17 -0700

    change color of histogram and add legend

--- gradio_cv_app.py
+++ gradio_cv_app.py
@@ -10,42 +10,24 @@ def create_mask(image):
     mask = np.ones((height, width), dtype=np.uint8) * 255
     return mask
 
-# def plot_histograms(image1, image2, image3):
-#     images = [image1, image2, image3]
-#     num_images = len(images)
-#     fig, axes = plt.subplots(num_images, 1, figsize=(6, 4*num_images))
-    
-#     for i, image in enumerate(images):
-#         greenmask = create_mask(image)
-#         hist_mask_values = cv2.calcHist([image], channels=[1], mask=greenmask, histSize=[256], ranges=[0, 256])
-        
-#         axes[i].plot(hist_mask_values, 'g')
-#         axes[i].set_title('Histogram - Image {}'.format(i+1))
-#         axes[i].set_xlabel('Pixel Value')
-#         axes[i].set_ylabel('Frequency')
-    
-#     plt.tight_layout()
-
-#     # Save the combined histogram plot as an image
-#     plt.savefig("histograms.png")
-#     plt.close()
-
-#     # Return the combined histogram plot image
-#     return "histograms.png"
 def plot_histograms(image1, image2):
     images = [image1, image2]
     num_images = len(images)
     fig, ax = plt.subplots(figsize=(8, 6))
 
-    for image in images:
+    colors = ['g', 'b']  # List of colors for different histograms
+    labels = ['Image 1', 'Image 2']  # List of labels for the legend
+
+    for i, image in enumerate(images):
         greenmask = create_mask(image)
         hist_mask_values = cv2.calcHist([image], channels=[1], mask=greenmask, histSize=[256], ranges=[0, 256])
         
-        ax.plot(hist_mask_values, 'g')
+        ax.plot(hist_mask_values, color=colors[i], label=labels[i])  # Plot each histogram with different color and label
     
     ax.set_title('Histogram Comparison of Multiple Images')
     ax.set_xlabel('Pixel Value')
     ax.set_ylabel('Frequency')
+    ax.legend()  # Display the legend
 
     plt.tight_layout()
 
@@ -55,6 +37,7 @@ def plot_histograms(image1, image2):
 
     # Return the histogram plot image
     return "histograms.png"
+
 # %%
 # Create the Gradio interface
 iface = gr.Interface(fn=plot_histograms,
